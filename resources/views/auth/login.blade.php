0a56d19705a51e82391127e1f02095eb7dd2623c
Aditya Prasetyo <adty404@gmail.com>
2021-03-18 16:33:28 +0700

    Login Page Integration

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,42 +1,11 @@
 @extends('layouts.auth')
 
+@section('title')
+    Store Login Page
+@endsection
+
 @section('content')
 <div class="page-content page-auth">
-    <div class="section-store-auth" data-aos="fade-up">
-        <div class="container">
-            <div class="row align-items-center row-login">
-                <div class="col-lg-6 text-center">
-                    <img src="/images/login-placeholder.png" alt="" class="w-50 mb-4 mb-lg-none" />
-                </div>
-                <div class="col-lg-5">
-                    <h2>
-                        Belanja kebutuhan utama, <br />
-                        menjadi lebih mudah
-                    </h2>
-                    <form class="mt-3">
-                        <div class="form-group">
-                            <label>Email address</label>
-                            <input type="email" class="form-control w-75" aria-describedby="emailHelp" />
-                        </div>
-                        <div class="form-group">
-                            <label>Password</label>
-                            <input type="password" class="form-control w-75" />
-                        </div>
-                        <a class="btn btn-success btn-block w-75 mt-4" href="/login.html">
-                            Sign In to My Account
-                        </a>
-                        <a class="btn btn-signup w-75 mt-2" href="/register.html">
-                            Sign Up
-                        </a>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-{{-- Page Login Default Laravel --}}
-<div class="page-content page-auth" style="display: none">
     <div class="section-store-auth" data-aos="fade-up">
         <div class="container">
             <div class="row align-items-center row-login">
