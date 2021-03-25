52d938518f04228c5b406b6108cec1720061f522
Aditya Prasetyo <adty404@gmail.com>
2021-03-25 17:55:01 +0700

    User Dashboard Page Integration

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -24,12 +24,34 @@
                     <img src="/images/dashboard-store-logo.svg" alt="" class="my-4" />
                 </div>
                 <div class="list-group list-group-flush">
-                    <a href="/dashboard.html" class="list-group-item list-group-item-action">Dashboard</a>
-                    <a href="/dashboard-products.html" class="list-group-item list-group-item-action">My Products</a>
-                    <a href="/dashboard-transactions.html"
-                        class="list-group-item list-group-item-action">Transactions</a>
-                    <a href="/dashboard-settings.html" class="list-group-item list-group-item-action">Store Settings</a>
-                    <a href="/dashboard-account.html" class="list-group-item list-group-item-action">My Account</a>
+                    <a href="{{ route('dashboard') }}"
+                        class="list-group-item list-group-item-action {{ (request()->is('dashboard')) ? 'active' : '' }} ">
+                        Dashboard
+                    </a>
+                    <a href="{{ route('dashboard-product') }}"
+                        class="list-group-item list-group-item-action {{ (request()->is('dashboard/products*')) ? 'active' : '' }} ">
+                        My Products
+                    </a>
+                    <a href="{{ route('dashboard-transaction') }}"
+                        class="list-group-item list-group-item-action {{ (request()->is('dashboard/transactions*')) ? 'active' : '' }} ">
+                        Transactions
+                    </a>
+                    <a href="{{ route('dashboard-settings-store') }}"
+                        class="list-group-item list-group-item-action {{ (request()->is('dashboard/settings*')) ? 'active' : '' }} ">
+                        Store Settings
+                    </a>
+                    <a href="{{ route('dashboard-settings-account') }}"
+                        class="list-group-item list-group-item-action {{ (request()->is('dashboard/account*')) ? 'active' : '' }} ">
+                        My Account
+                    </a>
+                    <a href="{{ route('logout') }}" onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();"
+                        class="list-group-item list-group-item-action">
+                        Sign Out
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
                 </div>
             </div>
             <!-- /#sidebar-wrapper -->
@@ -54,12 +76,16 @@
                                     aria-haspopup="true" aria-expanded="false">
                                     <img src="/images/icon-user.png" alt=""
                                         class="rounded-circle mr-2 profile-picture" />
-                                    Hi, Aditya
+                                        Hi, {{ Auth::user()->name }}
                                 </a>
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="/index.html">Back to Store</a>
-                                    <a class="dropdown-item" href="/dashboard-account.html">Settings</a>
+                                <div class="dropdown-menu">
+                                    <a href="{{ route('dashboard') }}" class="dropdown-item">Dashboard</a>
+
+                                    <a href="{{ route('dashboard-settings-account') }}"
+                                        class="dropdown-item">Settings</a>
+
                                     <div class="dropdown-divider"></div>
+
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                         onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                                         Logout
@@ -71,8 +97,16 @@
                                 </div>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link d-inline-block mt-2" href="#">
+                                <a href="{{ route('cart') }}" class="nav-link d-inline-block mt-2">
+                                    @php
+                                    $carts = \App\Cart::where('users_id', Auth::user()->id)->count();
+                                    @endphp
+                                    @if($carts > 0)
+                                    <img src="/images/icon-cart-filled.svg" alt="" />
+                                    <div class="card-badge">{{ $carts }}</div>
+                                    @else
                                     <img src="/images/icon-cart-empty.svg" alt="" />
+                                    @endif
                                 </a>
                             </li>
                         </ul>
@@ -80,7 +114,7 @@
                         <ul class="navbar-nav d-block d-lg-none mt-3">
                             <li class="nav-item">
                                 <a class="nav-link" href="#">
-                                    Hi, Aditya
+                                    Hi, {{ Auth::user()->name }}
                                 </a>
                             </li>
                             <li class="nav-item">
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -5,6 +5,7 @@ Store Dashboard
 @endsection
 
 @section('content')
+<!-- Section Content -->
 <div class="section-content section-dashboard-home" data-aos="fade-up">
     <div class="container-fluid">
         <div class="dashboard-heading">
@@ -22,7 +23,7 @@ Store Dashboard
                                 Customer
                             </div>
                             <div class="dashboard-card-subtitle">
-                                15,209
+                                {{ number_format($customer) }}
                             </div>
                         </div>
                     </div>
@@ -34,7 +35,7 @@ Store Dashboard
                                 Revenue
                             </div>
                             <div class="dashboard-card-subtitle">
-                                $931,290
+                                ${{ number_format($revenue) }}
                             </div>
                         </div>
                     </div>
@@ -46,7 +47,7 @@ Store Dashboard
                                 Transaction
                             </div>
                             <div class="dashboard-card-subtitle">
-                                22,409,399
+                                {{ number_format($transaction_count) }}
                             </div>
                         </div>
                     </div>
@@ -55,20 +56,23 @@ Store Dashboard
             <div class="row mt-3">
                 <div class="col-12 mt-2">
                     <h5 class="mb-3">Recent Transactions</h5>
-                    <a class="card card-list d-block" href="/dashboard-transactions-details.html">
+                    @foreach ($transaction_data as $transaction)
+                    <a href="{{ route('dashboard-transaction-details', $transaction->id) }}"
+                        class="card card-list d-block">
                         <div class="card-body">
                             <div class="row">
                                 <div class="col-md-1">
-                                    <img src="/images/dashboard-icon-product-1.png" alt="" />
+                                    <img src="{{ Storage::url($transaction->product->galleries->first()->photos ?? '') }}"
+                                        class="w-75" />
                                 </div>
                                 <div class="col-md-4">
-                                    Shirup Marzzan
+                                    {{ $transaction->product->name ?? '' }}
                                 </div>
                                 <div class="col-md-3">
-                                    Angga Risky
+                                    {{ $transaction->transaction->user->name ?? '' }}
                                 </div>
                                 <div class="col-md-3">
-                                    12 Januari, 2020
+                                    {{  $transaction->created_at ?? '' }}
                                 </div>
                                 <div class="col-md-1 d-none d-md-block">
                                     <img src="/images/dashboard-arrow-right.svg" alt="" />
@@ -76,48 +80,7 @@ Store Dashboard
                             </div>
                         </div>
                     </a>
-                    <a class="card card-list d-block" href="/dashboard-transactions-details.html">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-md-1">
-                                    <img src="/images/dashboard-icon-product-2.png" alt="" />
-                                </div>
-                                <div class="col-md-4">
-                                    LeBrone X
-                                </div>
-                                <div class="col-md-3">
-                                    Masayoshi
-                                </div>
-                                <div class="col-md-3">
-                                    11 January, 2020
-                                </div>
-                                <div class="col-md-1 d-none d-md-block">
-                                    <img src="/images/dashboard-arrow-right.svg" alt="" />
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                    <a class="card card-list d-block" href="/dashboard-transactions-details.html">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-md-1">
-                                    <img src="/images/dashboard-icon-product-3.png" alt="" />
-                                </div>
-                                <div class="col-md-4">
-                                    Soffa Lembutte
-                                </div>
-                                <div class="col-md-3">
-                                    Shayna
-                                </div>
-                                <div class="col-md-3">
-                                    11 January, 2020
-                                </div>
-                                <div class="col-md-1 d-none d-md-block">
-                                    <img src="/images/dashboard-arrow-right.svg" alt="" />
-                                </div>
-                            </div>
-                        </div>
-                    </a>
+                    @endforeach
                 </div>
             </div>
         </div>
